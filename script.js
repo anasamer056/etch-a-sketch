873b4ce7cde7db3f402b51fd03ff38585deec5d8
Aanas Aamer <anasamer056@gmail.com>
2023-11-08 23:28:25 +0200

    Add a button to allow the user to pick a custom board size

---
+++ script.js
@@ -0,0 +1,34 @@
+const squaresContainer = document.querySelector(".squares-container");
+const changeSizeButton = document.querySelector(".change-size");
+drawBoard();
+
+function drawBoard(squareCount = 16){
+    let squares = [...squaresContainer.childNodes];
+    if (squares.length > 0){
+        squares.forEach((square)=>squaresContainer.removeChild(square));
+    }
+
+    for (let i = 0; i < squareCount * squareCount; i++) {
+        let div = document.createElement("div");
+        div.classList.add("square", "inactive");
+        div.style.minWidth =  div.style.minHeight = `${squaresContainer.offsetWidth/squareCount}px`;
+        squaresContainer.appendChild(div);   
+    }
+}
+
+
+
+squaresContainer.addEventListener("mouseover", (event)=>{
+    if (event.target.classList.contains("squares-container")) return;
+    console.log("event captured")
+    event.target.classList.add("active");
+})
+
+changeSizeButton.addEventListener("click", ()=>{
+    let size;
+    do{
+        size = +prompt("How many squares per side? (Between 1 and 60)")
+        if (size == 0) size = 16;
+    } while (size <= 0 || size > 60);
+    drawBoard(size);
+})
