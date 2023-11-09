3b7b34655bc12148ecd93645c312e6f41a342ee6
Aanas Aamer <anasamer056@gmail.com>
2023-11-09 23:17:05 +0200

    Add Rainbow Mode
    Now the mouse pointer leaves a colorful trail instead of a white one

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Selecting elements
 const squaresContainer = document.querySelector(".squares-container");
-const changeSizeButton = document.querySelector(".change-size");
+const changeSizeBtn = document.querySelector(".change-size");
+const rainbowModeBtn = document.querySelector(".rainbow"); 
 
 // Main logic
 drawBoard();
@@ -14,24 +15,43 @@ function drawBoard(squareCount = 16){
 
     for (let i = 0; i < squareCount * squareCount; i++) {
         let div = document.createElement("div");
-        div.classList.add("square", "inactive");
+        div.classList.add("square");
+        div.style.backgroundColor = "black"
         div.style.minWidth =  div.style.minHeight = `${squaresContainer.offsetWidth/squareCount}px`;
         squaresContainer.appendChild(div);   
     }
 }
 
 // Event handler that colors a square on mouse hover
-squaresContainer.addEventListener("mouseover", (event)=>{
-    if (event.target.classList.contains("squares-container")) return;
-    console.log("event captured")
-    event.target.classList.add("active");
-})
+squaresContainer.addEventListener("mouseover", drawWhite)
 
-changeSizeButton.addEventListener("click", ()=>{
+function drawWhite (event) {
+    
+    if (event.target.classList.contains("squares-container")) return;
+    event.target.style.backgroundColor = "white"
+
+}
+
+changeSizeBtn.addEventListener("click", ()=>{
     let size;
     do{
         size = +prompt("How many squares per side? (Between 1 and 60)")
         if (size == 0) size = 16;
     } while (size <= 0 || size > 60);
     drawBoard(size);
-})
+});
+
+rainbowModeBtn.addEventListener("click", ()=>{
+    squaresContainer.removeEventListener("mouseover", drawWhite);
+    squaresContainer.addEventListener("mouseover", drawColors )
+});
+
+function drawColors(event) {
+    if (event.target.classList.contains("squares-container")) return;
+    let randRGB = [];
+    for (let i = 0; i < 3; i++) {
+        let num = Math.floor(Math.random() * 254 + 1);  
+        randRGB.push(num);
+    }
+    event.target.style.backgroundColor = `rgb(${randRGB})`
+}
