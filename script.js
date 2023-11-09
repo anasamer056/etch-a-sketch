41dfa3d99c34601ea94c42a2cecea0439251b43b
Aanas Aamer <anasamer056@gmail.com>
2023-11-09 22:54:20 +0200

    Add a couple of desctiptive comments

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// Selecting elements
 const squaresContainer = document.querySelector(".squares-container");
 const changeSizeButton = document.querySelector(".change-size");
+
+// Main logic
 drawBoard();
 
+// A function that creates the board based on the specified square count
 function drawBoard(squareCount = 16){
     let squares = [...squaresContainer.childNodes];
     if (squares.length > 0){
@@ -16,8 +20,7 @@ function drawBoard(squareCount = 16){
     }
 }
 
-
-
+// Event handler that colors a square on mouse hover
 squaresContainer.addEventListener("mouseover", (event)=>{
     if (event.target.classList.contains("squares-container")) return;
     console.log("event captured")
